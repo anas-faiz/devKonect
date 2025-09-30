9c5f68c6bc6234a33506ccdc0e846052831b3078
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-30 13:31:11 -0700

    userDatabase created + user added successfully

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const { connectDB } = require('./config/dataBase')
+const  User = require('./models/user')
 const app = express();
 
 const port = process.env.PORT || 4000;
 
+app.post("/signup",async (req,res)=>{
+    
+         
+    try{
+        const user = new User({
+        firstName: 'Anas',
+        lastName: 'faiz',
+        email: 'anas@faiz.com',
+        password: '123@anas',
+        });
+
+     await user.save();
+     res.send('user added successfully');
+    } catch(err){
+        res.status(404).send('user not added something went wrong: ' + err.message)
+    }
+})
 
 app.use("/",(err,req,res,next)=>{
     res.status(500).send("something went wrong")
