f87232c0bfedb1cca6fc75954ecdc08b70194805
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-30 12:24:54 -0700

    database added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { connectDB } = require('./config/dataBase')
 const app = express();
 
 const port = process.env.PORT || 4000;
@@ -23,6 +24,13 @@ app.use("/",(err,req,res,next)=>{
     res.status(500).send("something went wrong")
 })
 
-app.listen(port, () => {
+connectDB()
+    .then(()=>{
+        console.log('database established')
+    app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+    })    
+})
+.catch((err)=>{
+    console.log("dataBase not conncted")
+})
---
+++ src/config/dataBase.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectDB = async ()=>{
+    await mongoose.connect('mongodb+srv://anasfaiz0811_db_user:PFh4VWiN8N07NVt9@devkonect.cafen1v.mongodb.net/devKonect')
+}
+
+module.exports =  {connectDB};
