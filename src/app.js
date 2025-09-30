3ed8a3e0d19d4ad1554bce42b94be9c5fe967ea3
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-30 11:23:23 -0700

    middleware file added

--- src/app.js
+++ src/app.js
@@ -3,10 +3,23 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get("/" , (req,res)=>{
-    res.send('hello from server')
-})
+const { adminAuth, userAuth } = require('./middleWares/auth');
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`)
-})
+// Apply adminAuth for all /admin routes
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req, res) => {
+    res.send('Hello User');
+});
+
+app.get("/admin/allData", (req, res) => {
+    res.send('All Data Here');
+});
+
+app.get("/admin/deleteData", (req, res) => {
+    res.send('Data Deleted');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
