29cd763b2acdc560e3b639e25774762abd0a1c51
Anas-Faiz <anasfaiz0811@gmail.com>
2025-09-30 13:13:06 -0700

    removed test/practice routes

--- src/app.js
+++ src/app.js
@@ -4,22 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-const { adminAuth, userAuth } = require('./middleWares/auth');
 
-// Apply adminAuth for all /admin routes
-app.use("/admin", adminAuth);
-
-app.get("/user", userAuth, (req, res) => {
-    res.send('Hello User');
-});
-
-app.get("/admin/allData", (req, res) => {
-    res.send('All Data Here');
-});
-
-app.get("/admin/deleteData", (req, res) => {
-    res.send('Data Deleted');
-});
 app.use("/",(err,req,res,next)=>{
     res.status(500).send("something went wrong")
 })
