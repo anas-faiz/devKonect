dd116662d5e97b6eb6bce3a00e3ba02e7f80d8c9
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-01 10:30:42 -0700

    feed api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { connectDB } = require('./config/dataBase')
-const  User = require('./models/user')
+const  User = require('./models/user');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 const port = process.env.PORT || 4000;
@@ -10,11 +11,25 @@ app.use(express.json());
 app.post("/signup",async (req,res)=>{         
     //creating a new instance in the database 
         const user = new User(req.body);
-        try{
+     try{
      await user.save();
      res.send('user added successfully');
     } catch(err){
         res.status(404).send('user not added something went wrong: ' + err.message)
+    }
+})
+app.get("/feed",async(req,res)=>{
+    const user = req.body.email;
+    try{
+        const feed = await mongoose.find({})
+        if(!feed){
+            res.status(400).send("feed not detected")
+        }else{
+            res.send(feed); 
+        }
+
+    }catch(error){
+        res.status(404).send('Something went wrong: ' + error.message);
     }
 })
 
