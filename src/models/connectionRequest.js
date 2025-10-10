ad7a226ae7d5b8078945c1c16ce92b06bdeff04f
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-10 15:10:40 -0700

    connection request schema completed

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,17 +1,25 @@
-const mongooose = require("mongooose");
+const mongoose = require("mongooose");
 
 const connectionRequestSchema = new mongoose.Schema({
     fromUserId:{
-        type: mongoose.Schema.Types.ObjectId
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
     },
     toUserId:{
-        type: mongoose.Schema.Types.ObjectId
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
     },
     status:{
         type: String,
+        required: true,
         enum:{
             value:["accepted","interested","rejected","ignore"],
             message: "{value} is not a status type",
         }
     }
-})
+},{timestamps: true})
+
+
+const ConnectionRequestModel = new Mongoose.model("connectionRequest",connectionRequestSchema);
+
+module.exports = ConnectionRequestModel
