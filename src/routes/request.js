82fcc2500cc1027d1ed6ae317c2bab6242f679ba
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-12 00:20:06 -0700

    created request/send api

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,8 +3,13 @@ const { userAuth } = require("../middleWares/auth");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendconnectionrqst", userAuth, (req, res) => {
-  try {
+requestRouter.post("/request/send/:status/:toUserID", userAuth, (req, res) => {
+  try { 
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserID;
+    const status = req.params.status;
+
+    
 
   } catch (error) {
     res.status(404).send("error : " + error.message)
