bd1176e163f1c69642fc9c5a2052505b3c6f7e88
Anas-Faiz <anasfaiz0811@gmail.com>
2025-10-12 00:14:01 -0700

    removed fluff from send request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -5,9 +5,6 @@ const requestRouter = express.Router();
 
 requestRouter.post("/sendconnectionrqst", userAuth, (req, res) => {
   try {
-    const user = req.user;
-
-    res.send(user.firstName + " sent a connection request");
 
   } catch (error) {
     res.status(404).send("error : " + error.message)
