cf0dd5525715e814c53322f99e5f656fc33f8998
Anas-Faiz <anasfaiz0811@gmail.com>
2025-12-14 15:57:54 -0800

    .

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -1,5 +1,5 @@
-const cron = require ("node-cron")
+// const cron = require ("node-cron")
 
-cron.schedule("* * * * *", ()=>{
-    console.log ("hello")
-})
+// cron.schedule("* * * * *", ()=>{
+//     console.log ("hello " + new Date())
+// })
