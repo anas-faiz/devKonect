62a1f03646739b533d67c86a878fe3b7080c137d
Anas-Faiz <anasfaiz0811@gmail.com>
2025-12-28 22:23:20 -0800

    chat backend bug fix

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,35 +1,41 @@
-const express = require ("express");
+const express = require("express");
 const { userAuth } = require("../middleWares/auth");
 const Chat = require("../models/chat");
 
 const chatRouter = express.Router();
 
-chatRouter.get("/chat/:receiver", userAuth, async (req,res)=>{
-    try{    
+chatRouter.get("/chat/:receiver", userAuth, async (req, res) => {
+  try {
+    const { receiver } = req.params;
+    const sender = req.user._id;
 
-        const { receiver } = req.params;
+    let chat = await Chat.findOne({
+      participants: { $all: [sender, receiver] },
+    }).populate({
+      path: "messages.senderId",
+      select: "firstName lastName",
+    });
 
-        const sender = req.user._id;
+    if (!chat) {
+      chat = new Chat({
+        participants: [sender, receiver],
+        messages: [],
+      });
 
-        let chat = await Chat.findOne({
-            participants: { $all:[sender,receiver]}
-        });
+      await chat.save();
 
-        if(!chat){
-            chat = new Chat({
-                participants : [sender,receiver],
-                messages: [],
-            });
-
-            await chat.save();
-        }
-
-        res.json(chat);
-
-    }catch(err){
-        console.error(err)
+      chat = await Chat.findById(chat._id).populate({
+        path: "messages.senderId",
+        select: "firstName lastName",
+      });
     }
-})
 
+    res.status(200).json(chat);
+
+  } catch (err) {
+    console.error("❌ Fetch chat error:", err);
+    res.status(500).json({ message: "Failed to fetch chat" });
+  }
+});
 
 module.exports = chatRouter;
