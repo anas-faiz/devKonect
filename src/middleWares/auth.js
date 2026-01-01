0bf38e46d4fe42d1aad3f789b6f6eb4ccdb5cec1
Anas-Faiz <anasfaiz0811@gmail.com>
2025-12-31 23:14:31 -0800

    .8

--- src/middleWares/auth.js
+++ src/middleWares/auth.js
@@ -4,23 +4,23 @@ const User = require('../models/user');
 const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
-   
+
     if (!token) {
       return res.status(401).send("unauthorized");
     }
-  
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET );
-   
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
     const { _id } = decodedToken;
-    
+
     const user = await User.findById({ _id });
-    
+
     if (!user) {
       throw new Error("User not found");
     }
-    
+
     req.user = user;
-    
+
     next();
   } catch (error) {
     res.status(400).send("Error : " + error.message);
